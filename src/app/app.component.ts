46e55a2f2a9e88add09a2340c59e99ae197c2fe2
LuisSoria31 <utm23090484@utma.edu.mx>
2025-02-06 09:30:50 -0600

    Aqui esta mi proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { CardComponent } from './card/card.component';
+import { InformacionComponent } from './informacion/informacion.component';
+import { PieComponent } from './pie/pie.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EncabezadoComponent, ContenidoComponent, CardComponent, InformacionComponent, PieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { CardComponent } from './card/card.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ContenidoComponent},
+    {path: 'Compra', component: CardComponent},
+    {path: 'Informacion', component: InformacionComponent}
+];
